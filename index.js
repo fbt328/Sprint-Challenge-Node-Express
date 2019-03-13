613de460a6b66f0be0c0ca2d35d81ecbbe539731
fbt328 <katherine.rust.andrews@gmail.com>
2019-03-13 12:10:39 -0700

    initial project setup, dependencies added and server set up to listen

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
 
-// code away!
+const server = require('./server.js')
+const PORT = 9090;
+
+server.listen(PORT, () => {
+    console.log(`server is now listening on port ${PORT}!`);
+})
